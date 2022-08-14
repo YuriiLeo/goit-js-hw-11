df6406a8aa3b46105769453cc400dcb00819cc54
YuriiLeo <yuriileontenko@gmail.com>
2022-08-14 21:23:02 +0300

    add render page

--- src/js/index.js
+++ src/js/index.js
@@ -1,28 +1,67 @@
 import '../css/styles.css';
+import Pixabay from './pixabayAPI'; "./pixabayAPI"
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-// const KEY = "29252112-5cfbcf527b6aa7a1ff4768ca5";
-// const BASE_URL = https://pixabay.com/api/;
+const refs = {
+  searchForm: document.querySelector(".search-form"),
+  // inputForm: document.querySelector(".input"),
+  // btnSubmit: document.querySelector(".btn-submit"),
+  btnLoadMore: document.querySelector(".load-more"),
+  gallery: document.querySelector(".gallery"),
 
-// https://pixabay.com/api/?key=29252112-5cfbcf527b6aa7a1ff4768ca5&q=cat&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=1
+};
 
-{/* <div class="photo-card">
-        <img src="" alt="" loading="lazy" />
+const pixabay = new Pixabay();
+
+const KEY = "29252112-5cfbcf527b6aa7a1ff4768ca5";
+const BASE_URL = "https://pixabay.com/api/";
+
+refs.searchForm.addEventListener("submit", onSearch);
+refs.btnLoadMore.addEventListener("click", onLoadMore);
+
+function onSearch(evt) {
+  evt.preventDefault();
+  // removeData();
+
+  pixabay.searchQuery = evt.currentTarget.elements.searchQuery.value;
+  pixabay.resetPage();
+  pixabay.fetchArticles(BASE_URL, KEY).then(renderGallery);
+}
+
+function onLoadMore() {
+  pixabay.fetchArticles(BASE_URL, KEY).then(renderGallery);
+} 
+
+function renderGallery(hits) {
+ console.log("dani", hits);
+  const markupPictures = hits.reduce((acc, item) => (acc += `
+      <div class="photo-card">
+        <img src="${item.webformatURL}" alt="${item.tags}" loading="lazy" />
         <div class="info">
           <p class="info-item">
-            <b>Likes</b>
+            <b>Likes${item.likes}</b>
           </p>
           <p class="info-item">
-            <b>Views</b>
+            <b>Views${item.views}</b>
           </p>
           <p class="info-item">
-            <b>Comments</b>
+            <b>Comments${item.comments}</b>
           </p>
           <p class="info-item">
-            <b>Downloads</b>
+            <b>Downloads${item.downloads}</b>
           </p>
         </div>
-      </div> */}
+      </div>
+       `), "");
+
+       refs.gallery.insertAdjacentHTML ("beforeend", markupPictures);
+}
+
+
+// function removeData() {
+//     refs.gallery.innerHTML = "";
+// }
+
 
 // Якщо бекенд повертає порожній масив, значить нічого підходящого не було знайдено. 
 // У такому разі показуй повідомлення з текстом "Sorry, there are no images matching your search query. Please try again."
