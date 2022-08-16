02903beb3626b95f92a8f1d030cb41edc3ba4aff
YuriiLeo <yuriileontenko@gmail.com>
2022-08-16 23:54:37 +0300

    add working code

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,9 @@
 import '../css/styles.css';
-import Pixabay from './pixabayAPI'; "./pixabayAPI"
+import Pixabay from './pixabayAPI';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const refs = {
   searchForm: document.querySelector(".search-form"),
-  // inputForm: document.querySelector(".input"),
-  // btnSubmit: document.querySelector(".btn-submit"),
   btnLoadMore: document.querySelector(".load-more"),
   gallery: document.querySelector(".gallery"),
 
@@ -13,30 +11,55 @@ const refs = {
 
 const pixabay = new Pixabay();
 
-const KEY = "29252112-5cfbcf527b6aa7a1ff4768ca5";
-const BASE_URL = "https://pixabay.com/api/";
+isHidden();
+
+function isHidden() {
+  refs.btnLoadMore.classList.add("is-hidden");
+}
 
 refs.searchForm.addEventListener("submit", onSearch);
 refs.btnLoadMore.addEventListener("click", onLoadMore);
 
-function onSearch(evt) {
-  evt.preventDefault();
-  // removeData();
-
+ function onSearch(evt) {
+  evt.preventDefault(pixabay.searchQuery);
+  refs.btnLoadMore.classList.remove("is-hidden");
   pixabay.searchQuery = evt.currentTarget.elements.searchQuery.value;
+  
+  if (pixabay.searchQuery === "") {
+    return Notify.warning("Please enter a more specific name.");
+  }
+  
+  //  Notify.warning(`Hooray! We found ${pixabay.totalHitsThis}images.`);
+   
   pixabay.resetPage();
-  pixabay.fetchArticles(BASE_URL, KEY).then(renderGallery);
+  fetchArticlesAndRender();
+  removeData();
+  return;
 }
 
+
 function onLoadMore() {
-  pixabay.fetchArticles(BASE_URL, KEY).then(renderGallery);
+  fetchArticlesAndRender();
+  totalHitsCaunter();
+  return;
 } 
 
-function renderGallery(hits) {
- console.log("dani", hits);
-  const markupPictures = hits.reduce((acc, item) => (acc += `
+async function fetchArticlesAndRender() {
+  try {
+    const fetchArticles = await pixabay.fetchArticles();
+    await renderGallery(fetchArticles);
+
+  } catch (error) {
+    console.log(error);
+    
+  }
+}
+
+function renderGallery(data) {
+  
+  const markupPictures = data.hits.reduce((acc, item) => (acc += `
       <div class="photo-card">
-        <img src="${item.webformatURL}" alt="${item.tags}" loading="lazy" />
+        <img src="${item.webformatURL}" alt="${item.tags}" loading="lazy"/>
         <div class="info">
           <p class="info-item">
             <b>Likes${item.likes}</b>
@@ -54,20 +77,25 @@ function renderGallery(hits) {
       </div>
        `), "");
 
-       refs.gallery.insertAdjacentHTML ("beforeend", markupPictures);
+  refs.gallery.insertAdjacentHTML("beforeend", markupPictures);
 }
 
 
-// function removeData() {
-//     refs.gallery.innerHTML = "";
-// }
+function removeData() {
+    refs.gallery.innerHTML = "";
+}
 
+function totalHitsCaunter() {
+  let maxHits = (pixabay.page) * 40;
+  let totalHits = pixabay.totalHitsThis;
+  
+  if (maxHits >= totalHits) {
+    Notify.warning("We're sorry, but you've reached the end of search results.");
+    isHidden();
+  }
+  return; 
+}
 
-// Якщо бекенд повертає порожній масив, значить нічого підходящого не було знайдено. 
-// У такому разі показуй повідомлення з текстом "Sorry, there are no images matching your search query. Please try again."
-
-// У відповіді бекенд повертає властивість totalHits - загальна кількість зображень, які відповідають критерію пошуку (для безкоштовного акаунту). 
-// Якщо користувач дійшов до кінця колекції, ховай кнопку і виводь повідомлення з текстом "We're sorry, but you've reached the end of search results.".
 
 // Після першого запиту з кожним новим пошуком отримувати повідомлення, в якому буде написано,
 //  скільки всього знайшли зображень(властивість totalHits).Текст повідомлення - "Hooray! We found totalHits images."
--- src/js/pixabayAPI.js
+++ src/js/pixabayAPI.js
@@ -1,21 +1,31 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+const axios = require('axios').default;
+
+const KEY = "29252112-5cfbcf527b6aa7a1ff4768ca5";
+const BASE_URL = "https://pixabay.com/api/";
+    
 export default class PixabayAPI {
+    
     constructor() { 
         this.searchQuery = "";
         this.page = 1;
+        this.totalHitsThis = 0;
     }
 
-    fetchArticles(BASE_URL, KEY, searchQuery) {
+  async  fetchArticles() {
         
   const url = `${BASE_URL}?key=${KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}`
   
- return fetch(url)
-  .then(response => response.json())
-      .then(data => {
-      console.log(this);
-      this.page += 1;
-          console.log(data);
-          return data.hits;
-     });
+      const response = await fetch(url);
+      const data = await response.json();
+
+          this.totalHitsThis = data.totalHits;
+          if (data.hits.length === 0) {
+             Notify.warning("Sorry, there are no images matching your search query. Please try again.");
+          }
+            this.page += 1;
+          return data;
+   
     }
     
     resetPage() {
@@ -29,4 +39,12 @@ export default class PixabayAPI {
     set query(newQuery) {
         this.searchQuery = newQuery;
     }
+
+    //  get page() {
+    //     return this.page;
+    // }
+
+    // set page(newPage) {
+    //     this.page = newPage;
+    // }
 }
